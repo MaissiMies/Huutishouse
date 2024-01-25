872c94f5b9e46f4bf4c66409bfa2584da4082c25
Rami Ahto <Rami.Ahto@edu.savonia.fi>
2024-01-25 10:54:57 +0200

    korjattu kirjoitusvirheitä

--- huutokauppa-app/backend/index.js
+++ huutokauppa-app/backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const router = require('./routes/router.js')
+const router = require('./routes/router')
 
 const app = express()
 
@@ -20,6 +20,6 @@ app.use('/', router)
 //lisää src package.jsoniin "proxy":http://localhost:3001,
 const port = 3001
 const server = app.listen(port,() => {
-    consaole.log(`Server running on port ${port}`)
+    console.log(`Server running on port ${port}`)
 })
 
