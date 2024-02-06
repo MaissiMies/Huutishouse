f8b82a24af5d90f1ce883677cc920c11820f71d7
Tuukka Hänninen <Tuukka.Hanninen@edu.savonia.fi>
2024-02-06 12:26:08 +0200

    Layout

--- huutokauppa-app/src/App.js
+++ huutokauppa-app/src/App.js
@@ -3,36 +3,43 @@ import './App.css';
 import Header from './Komponentit/Header';
 import Myynti from './sivut/myynti' //<---esim myynti
 import Kotisivu from './sivut/kotisivu'
-import {BrowserRouter, Routes, Route, Outlet} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
 
 //routet pitäisi laittaa omaan tiedostoonsa,myös frontendissä esim komponentit/routes yms
 //routet käytännössä linkkejä, esim myynti haetaan hakemistosta, ja nimetään. käytetään sitten routessa elementtinä.
 //<outletin /> alle <Footer /> kun tehty
 function App() {
-const Layout = () =>{
-  return(
-  <>
-    <Header />
-    <Outlet />
-    
-  </>
-  )
-}
+const Layout = ({ children }) => {
   return (
-    <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        
-      <Route path="/" element={<Kotisivu />} />
-      <Route path="myynti" element={<Myynti />} />
-
-    </Route>
-
-    </Routes>
-    </BrowserRouter>
-    </>
+    <div className="layout">
+      <header />
+      <div className="container">
+        <div className="sidebar">
+          {/* Sidebar content goes here */}
+          <ul>
+            <li><a href="/myynti">Myynti</a></li>
+            <li><a href="/">Kotisivu</a></li>
+            <li>Link 3</li>
+          </ul>
+        </div>
+        <main className="main-content">{children}</main>
+      </div>
+      <footer />
+    </div>
   );
-}
+};
+
+return (
+  <Router>
+    <Routes>
+    <Route path="/" element={<Layout><Kotisivu /></Layout>} />
+    <Route path="/myynti" element={<Layout><Myynti /></Layout>} />
+      
+     
+    </Routes>
+  </Router>
+);
+};
+
 
 export default App;
