fa48956b71761bf3fd78c6769015b9c1983dd1c5
Tuukka Hänninen <Tuukka.Hanninen@edu.savonia.fi>
2024-03-26 22:07:28 +0200

    Fronted tuotedelete

--- huutokauppa-app/frontend/src/sivut/Adminsivu.js
+++ huutokauppa-app/frontend/src/sivut/Adminsivu.js
@@ -9,6 +9,8 @@ import { KayttajaUL } from '../Komponentit/KayttajaList';
 function Adminsivu(){
 const [access, setaccess] = useState(false)
 const { user } = useAuth();
+const [products, setProducts] = useState([]);
+const [deleteSuccess, setDeleteSuccess] = useState(false);
 
 
 useEffect(() => {
@@ -33,8 +35,56 @@ useEffect(() => {
     fetchData();
   }, []);
 
+ const handleDeleteproduct = async (id) => {
+  const confirmDelete = window.confirm('Haluatko varmasti poistaa Tuotteen?');
+    if (confirmDelete) {
+      try {
+        await axios.delete(`/tuotteet/delete/:${id}`);
+        // Poista käyttäjä taulukosta
+        setProducts(products.filter(user => products._id !== id));
+        setDeleteSuccess(true); // Näytä onnistumisviesti
+        setTimeout(() => {
+          setDeleteSuccess(false); // Piilota viesti ajan kuluttua
+        }, 3000); // Aika millisekunteina, esim. 3000 = 3 sekuntia
+        console.log('Käyttäjä poistettu onnistuneesti');
+      } catch (error) {
+        console.error('Virhe käyttäjän poistossa:', error);
+      }
+    }
+  };
+
  return(
+  <>
   <KayttajaUL/>
+  <div>
+      <h1>Product List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>nimi</th>
+            <th>kategoria</th>
+            <th>Viimeisin hinta</th>
+            <th>kayttaja</th>
+            {/* Add more table headings as needed */}
+          </tr>
+        </thead>
+        <tbody>
+          {products.map(product => (
+            <tr key={product._id}>
+              <td>{product.nimi}</td>
+              <td>{product.kategoria}</td>
+              <td>{product.lahtohinta}</td>
+              <td>{product.kayttajaid}</td>
+              <button onClick={() => handleDeleteproduct(product._id)}>Poista käyttäjä</button>
+              {/* Add more table data cells as needed */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </>
+
+  
  )
 }
 export default Adminsivu
